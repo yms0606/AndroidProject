c7531f61aaea7f3477e2b66bae7d4dcb3a6c2f04
chamny20 <dge3179@ajou.ac.kr>
2021-04-28 19:36:30 +0900

    Update DiaryActivity.java

--- AndroidProject_team/app/src/main/java/com/example/androidproject_team/DiaryActivity.java
+++ AndroidProject_team/app/src/main/java/com/example/androidproject_team/DiaryActivity.java
@@ -3,19 +3,29 @@ package com.example.androidproject_team;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 public class DiaryActivity extends AppCompatActivity {
     private static final int REQUEST_CODE = 0;
     private ImageView img1,img2,img3,img4;
+    String shared = "file";
+    EditText title, content;
+    int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,9 +43,76 @@ public class DiaryActivity extends AppCompatActivity {
                 intent.setType("image/*");
                 intent.setAction(intent.ACTION_GET_CONTENT);
                 startActivityForResult(intent,REQUEST_CODE);
+                i = 1;
             }
         });
+        img2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent,REQUEST_CODE);
+                i = 2;
+            }
+        });
+        img3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent,REQUEST_CODE);
+                i = 3;
+            }
+        });
+        img4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent,REQUEST_CODE);
+                i = 4;
+            }
+        });
+
+
+        title = (EditText)findViewById(R.id.title);
+        content = (EditText)findViewById(R.id.content);
+//        save.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View view) {
+//
+//            }
+//        });
+
+        SharedPreferences sharedPreferences = getSharedPreferences(shared, 0);
+        String value1 = sharedPreferences.getString("txttitle", "");
+        String value2 = sharedPreferences.getString("txtcontent", "");
+
+        title.setText(value1);
+        content.setText(value2);
+
+
+
+
     }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        SharedPreferences sharedPreferences = getSharedPreferences(shared, 0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        String value1 = title.getText().toString();
+        String value2 = content.getText().toString();
+        editor.putString("txttitle", value1);
+        editor.putString("txtcontent", value2);
+
+        editor.commit();
+    }
+
+
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -45,9 +122,20 @@ public class DiaryActivity extends AppCompatActivity {
 
                 try {
                     InputStream in = getContentResolver().openInputStream(data.getData());
-                    Bitmap img = BitmapFactory.decodeStream(in);
+                    Bitmap img= BitmapFactory.decodeStream(in);
                     in.close();
-                    img1.setImageBitmap(img);
+
+                    switch(i) {
+                        case 1:
+                            img1.setImageBitmap(img);break;
+                        case 2:
+                            img2.setImageBitmap(img);break;
+                        case 3:
+                            img3.setImageBitmap(img);break;
+                        case 4:
+                            img4.setImageBitmap(img);break;
+                        default: break;
+                    }
                 }catch (Exception e){}
             }
             else if(resultCode == RESULT_CANCELED){
@@ -55,4 +143,6 @@ public class DiaryActivity extends AppCompatActivity {
             }
         }
     }
+
+
 }
