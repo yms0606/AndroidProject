7fadf1518ded0cc89a44974092ae8376ae6e3163
seorinn <80616701+seorinn@users.noreply.github.com>
2021-04-28 18:06:09 +0900

    Update and rename JunjuActivity.java to JeollaActivity.java

--- AndroidProject_team/app/src/main/java/com/example/androidproject_team/JeollaActivity.java
+++ AndroidProject_team/app/src/main/java/com/example/androidproject_team/JeollaActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class JunjuActivity extends AppCompatActivity {
+public class JeollaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_junju);
+        setContentView(R.layout.activity_jeolla);
     }
 }
