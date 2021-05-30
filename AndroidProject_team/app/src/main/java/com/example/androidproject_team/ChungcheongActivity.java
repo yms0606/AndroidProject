71fb5b051d6b51dc558fe157da564b96db49b5e0
chamny20 <dge3179@ajou.ac.kr>
2021-05-31 06:00:23 +0900

    Update ChungcheongActivity.java
    Update button

--- AndroidProject_team/app/src/main/java/com/example/androidproject_team/ChungcheongActivity.java
+++ AndroidProject_team/app/src/main/java/com/example/androidproject_team/ChungcheongActivity.java
@@ -10,5 +10,14 @@ public class ChungcheongActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chungcheong);
+        
+        Button btSejong = findViewById(R.id.sejong);
+        btSejong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),ListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
