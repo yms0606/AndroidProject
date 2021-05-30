f42f2e9c3f0e8c9c1b88976283e679789ccbe55e
chamny20 <dge3179@ajou.ac.kr>
2021-05-31 06:02:08 +0900

    Update GyeongsangActivity.java
    update button

--- AndroidProject_team/app/src/main/java/com/example/androidproject_team/GyeongsangActivity.java
+++ AndroidProject_team/app/src/main/java/com/example/androidproject_team/GyeongsangActivity.java
@@ -10,5 +10,30 @@ public class GyeongsangActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gyeongsang);
+        
+        Button btBusan = findViewById(R.id.busan);
+        btBusan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),ListActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btDaegu = findViewById(R.id.daegu);
+        btDaegu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),ListActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btUlsan = findViewById(R.id.ulsan);
+        btUlsan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),ListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
