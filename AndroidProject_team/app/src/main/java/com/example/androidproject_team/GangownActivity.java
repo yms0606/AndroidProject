1ad758dbc972886de91deccb6ea7723ed3831d4e
chamny20 <dge3179@ajou.ac.kr>
2021-05-31 06:01:10 +0900

    Update GangownActivity.java
    update button

--- AndroidProject_team/app/src/main/java/com/example/androidproject_team/GangownActivity.java
+++ AndroidProject_team/app/src/main/java/com/example/androidproject_team/GangownActivity.java
@@ -10,5 +10,47 @@ public class GangownActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gangown);
+        
+        Button btGangneung = findViewById(R.id.gangneung);
+        btGangneung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),ListActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btSokcho = findViewById(R.id.sokcho);
+        btSokcho.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),ListActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btInje = findViewById(R.id.inje);
+        btInje.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),ListActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btPyeongchang = findViewById(R.id.pyeongchang);
+        btPyeongchang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),ListActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btWonju = findViewById(R.id.wonju);
+        btWonju.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),ListActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
