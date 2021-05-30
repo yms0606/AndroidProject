f08f99df02f60c761febc95dc60826e1615884ed
chamny20 <dge3179@ajou.ac.kr>
2021-05-31 06:01:43 +0900

    Update GyeonggiActivity.java
    update button

--- AndroidProject_team/app/src/main/java/com/example/androidproject_team/GyeonggiActivity.java
+++ AndroidProject_team/app/src/main/java/com/example/androidproject_team/GyeonggiActivity.java
@@ -10,5 +10,22 @@ public class GyeonggiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gyeonggi);
+        
+        Button btSuwonhwasung = findViewById(R.id.suwonhwasung);
+        btSuwonhwasung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),ListActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btJunmang = findViewById(R.id.junmang);
+        btJunmang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),ListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
