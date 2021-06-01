9ad6d8a3ab5c23752c866f287ae83d1c96a66251
chamny20 <dge3179@ajou.ac.kr>
2021-06-01 16:33:15 +0900

    Update SplashActivity

---
+++ AndroidProject_team/app/src/main/java/com/example/androidproject_team/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.androidproject_team;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler hd = new Handler();
+        hd.postDelayed(new splashhandler(), 3000);
+
+    }
+
+    private class splashhandler implements  Runnable {
+        public void run() {
+            startActivity(new Intent(getApplication(), MainActivity.class));
+            SplashActivity.this.finish();
+        }
+    }
+    @Override
+    public void onBackPressed() {
+
+    }
+}
