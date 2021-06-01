7f390f286d98cee30596b29e02071be7dda1bbad
seorinn <80616701+seorinn@users.noreply.github.com>
2021-06-01 15:30:40 +0900

    Todo and Quest

--- AndroidProject_team/app/src/main/java/com/example/androidproject_team/QuestFragment.java
+++ AndroidProject_team/app/src/main/java/com/example/androidproject_team/QuestFragment.java
@@ -1,22 +1,31 @@
-package com.example.androidproject_team;
+package com.example.myapplication;
 
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.androidproject_team.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
-public class QuestFragment extends Fragment {
-
-
+public class QuestFragment extends Fragment{
 
     public QuestFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,6 +35,36 @@ public class QuestFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_quest, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_quest, container, false);
+
+        String[] items = {"QuestA", "QuestB", "QuestC", "QuestD"};
+        ListView listview = (ListView) v.findViewById(R.id.questlistview);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_single_choice, items);
+        listview.setAdapter(adapter);
+
+        Button buttonSave = (Button) v.findViewById(R.id.buttonSave);
+        buttonSave.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SparseBooleanArray checkedItems = listview.getCheckedItemPositions();
+                int count = adapter.getCount();
+                int value = 0;
+
+                for (int i = count - 1; i >= 0; i--) {
+                    if (checkedItems.get(i)) {
+                        value += 1;
+                    }
+                }
+                int perc = (value/count)*100;
+                ProgressBar progress = (ProgressBar) v.findViewById(R.id.progress) ;
+//                progress.setIndeterminate(true);
+                progress.setProgress(perc) ;
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        return v;
     }
+
 }
--- AndroidProject_team/app/src/main/java/com/example/androidproject_team/TodoFragment.java
+++ AndroidProject_team/app/src/main/java/com/example/androidproject_team/TodoFragment.java
@@ -1,30 +1,180 @@
-package com.example.androidproject_team;
+
+package com.example.myapplication;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.androidproject_team.R;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
 
 public class TodoFragment extends Fragment {
-
-
-    public TodoFragment() {
-
-    }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_todo, container, false);
+        ListView listview;
+        ArrayAdapter adapter;
+        ArrayList<String> items = new ArrayList<>();
+
+        View v = inflater.inflate(R.layout.fragment_todo, container, false);
+
+        listview = (ListView) v.findViewById(R.id.todoListView);
+        adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_single_choice, items);
+
+        listview.setAdapter(adapter);
+
+        Button buttonAdd = (Button) v.findViewById(R.id.buttonAdd);
+        Button buttonDelete = (Button) v.findViewById(R.id.buttonDelete);
+        Button buttonSave = (Button) v.findViewById(R.id.buttonSave2);
+        buttonAdd.setEnabled(false);
+
+        adapter.notifyDataSetChanged();
+
+        buttonAdd.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText editTextNew = (EditText) getView().findViewById(R.id.editText);
+                String strNew = (String) editTextNew.getText().toString();
+
+                if (strNew.length() != 0) {
+                    items.add(strNew);
+                    editTextNew.setText("");
+                    adapter.notifyDataSetChanged();
+//                    saveItems() ;
+                }
+            }
+        });
+
+        buttonDelete.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SparseBooleanArray checkedItems = listview.getCheckedItemPositions();
+
+                int count = adapter.getCount();
+
+                for (int i = count - 1; i >= 0; i--) {
+                    if (checkedItems.get(i)) {
+                        items.remove(i);
+                    }
+                }
+                listview.clearChoices();
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        buttonSave.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SparseBooleanArray checkedItems = listview.getCheckedItemPositions();
+                int count = adapter.getCount();
+                int value = 0;
+
+                for (int i = count - 1; i >= 0; i--) {
+                    if (checkedItems.get(i)) {
+                        value += 1;
+                    }
+                }
+                ProgressBar progress = (ProgressBar) v.findViewById(R.id.progress) ;
+                progress.setProgress((value/count)*100) ;
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        EditText editText = (EditText) v.findViewById(R.id.editText);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable edit) {
+                Button buttonAdd = (Button) v.findViewById(R.id.buttonAdd);
+                if (edit.toString().length() > 0)
+                    buttonAdd.setEnabled(true);
+                else
+                    buttonAdd.setEnabled(false);
+            }
+        });
+
+        return v;
     }
+
+//    private final String fileName = "items.list";
+    //        private void saveItems() {
+//        File file = new File(getFilesDir(), fileName);
+//        FileWriter fw = null;
+//        BufferedWriter bufwr = null;
+//
+//        try {
+//            fw = new FileWriter(file);
+//            bufwr = new BufferedWriter(fw);
+//
+//            for (String str : items) {
+//                bufwr.write(str);
+//                bufwr.newLine();
+//            }
+//
+//            bufwr.flush();
+//        } catch (Exception e) {
+//        }
+//
+//        try {
+//            if (bufwr != null)
+//                bufwr.close();
+//            if (fw != null)
+//                fw.close();
+//        } catch (Exception e) {
+//        }
+//    }
+//
+//    private void loadItems() {
+//        File file = new File(getFilesDir(), fileName);
+//        FileReader fr = null;
+//        BufferedReader bufrd = null;
+//        String str;
+//
+//        if (file.exists()) {
+//            try {
+//                fr = new FileReader(file);
+//                bufrd = new BufferedReader(fr);
+//
+//                while ((str = bufrd.readLine()) != null) {
+//                    items.add(str);
+//                }
+//
+//                bufrd.close();
+//                fr.close();
+//            } catch (Exception e) {
+//            }
+//        }
+//    }
 }
